3ad62cd6938d1c72382b67df3e42a6a24886c986
Greg Bowler <greg.bowler@g105b.com>
2022-12-06 14:07:35 +0000

    feature: output config variable to page

--- page/index.php
+++ page/index.php
@@ -1,13 +1,16 @@
 <?php
+use Gt\Config\Config;
 use Gt\DomTemplate\DocumentBinder;
 use Gt\Http\Response;
 use Gt\Input\Input;
 use Gt\Session\Session;
 
-function go(Session $session, DocumentBinder $binder):void {
+function go(Session $session, DocumentBinder $binder, Config $config):void {
 	if($name = $session->getString("your-name")) {
 		$binder->bindKeyValue("your-name", $name);
 	}
+
+	$binder->bindKeyValue("configMessage", $config->getString("example.test_key"));
 }
 
 function do_greet(Input $input, Session $session, Response $response):void {
